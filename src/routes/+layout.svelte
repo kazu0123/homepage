72951169f08e2d0b85a987765b64fa13a3829723
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-26 15:11:26 +0900

    ナビゲーションバーの追加

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,60 @@
+<style>
+    :global(body) {
+        margin: 0;
+    }
+
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 1rem;
+        background-color: #f8f9fa;
+        border-bottom: 1px solid #ddd;
+    }
+
+    nav ul {
+        display: flex;
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    nav ul li {
+        margin-right: 1rem;
+    }
+
+    nav ul li:last-child {
+        margin-right: 0;
+    }
+
+    nav a {
+        text-decoration: none;
+        color: #333;
+        transition: color 0.3s;
+    }
+
+    nav a:hover {
+        color: #007bff;
+    }
+
+    div.name {
+        font-weight: bold;
+    }
+</style>
+
+<header>
+    <div class="name">小久保和喜</div>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/#about-me">About Me</a></li>
+            <li><a href="/#projects">Projects</a></li>
+            <li><a href="/#skills">Skills</a></li>
+            <li><a href="/#contact">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main>
+    <slot />
+</main>

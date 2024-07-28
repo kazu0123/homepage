16bf4ff11e5f94c9e267850f27560a694739b0ee
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-28 17:48:12 +0900

    fix contentful.ts

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,4 +1,5 @@
-import { createClient } from 'contentful';
+import contentful from 'contentful';
+const {createClient} = contentful;
 
 import { CONTENTFUL_SPACE_ID, CONTENTFUL_DELIVERY_TOKEN } from '$env/static/private';
 

50068f4f9f9bf0f779853c83cb783c1eb0df04fb
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-29 01:37:27 +0900

    add isResolvedTechnologyEntry function

--- src/lib/Technology.ts
+++ src/lib/Technology.ts
@@ -1,4 +1,4 @@
-import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode, UnresolvedLink } from "contentful";
 
 export interface TechnologyFields {
   name: EntryFieldTypes.Symbol;
@@ -7,3 +7,16 @@ export interface TechnologyFields {
 
 export type TechnologySkeleton = EntrySkeletonType<TechnologyFields, "technology">;
 export type Technology<Modifiers extends ChainModifiers, Locales extends LocaleCode = LocaleCode> = Entry<TechnologySkeleton, Modifiers, Locales>;
+
+
+export function isResolvedTechnologyEntry(
+  entry: UnresolvedLink<"Entry"> | Entry<TechnologySkeleton, undefined, string> | undefined
+): entry is Entry<TechnologySkeleton, undefined, string> {
+  if (typeof entry == "undefined") return false
+
+  const resolvedEntry = entry as Entry<TechnologySkeleton, undefined, string>;
+
+  if (typeof resolvedEntry == "undefined") return false
+
+  return true
+}

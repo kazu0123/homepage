41554a08d74315ab6ea642903ba82e38bc72a96d
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-29 01:36:33 +0900

    add isResolvedAssetWithFile function

---
+++ src/lib/isResolvedAssetWithFile.ts
@@ -0,0 +1,26 @@
+import type { Asset, UnresolvedLink } from 'contentful';
+
+
+export function isResolvedAssetWithFile(
+    asset: UnresolvedLink<"Asset"> | Asset<undefined, string> | undefined
+): asset is Asset<undefined, string> & { fields: { file: { url: string } } } {
+    if (!asset) {
+        return false;
+    }
+
+    const resolvedAsset = asset as Asset<undefined, string>;
+
+    if (!resolvedAsset.fields) {
+        return false;
+    }
+
+    if (!resolvedAsset.fields.file) {
+        return false;
+    }
+
+    if (!resolvedAsset.fields.file.url) {
+        return false;
+    }
+
+    return true;
+}

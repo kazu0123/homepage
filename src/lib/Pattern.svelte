e20e364b2469db39142e2f40d7e5d4019ef8d47a
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-31 00:04:33 +0900

    Trianglifyを追加

---
+++ src/lib/Pattern.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+  import trianglify from "trianglify";
+
+  export let width = 1000;
+  export let height = 1000;
+  export let cellSize = 100;
+  export let seed = "example-seed";
+
+  let patternCanvas: HTMLCanvasElement
+
+  function generatePattern() {
+    const pattern = trianglify({ width, height, cellSize, seed, });
+    pattern.toCanvas(patternCanvas);
+  }
+
+  onMount(generatePattern);
+</script>
+
+<canvas bind:this={patternCanvas} />

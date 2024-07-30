adba273371b44a432c4ab114fa096118d06c1842
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-31 00:10:09 +0900

    型エラーを修正

--- src/lib/Pattern.svelte
+++ src/lib/Pattern.svelte
@@ -2,10 +2,10 @@
 	import { onMount } from "svelte";
   import trianglify from "trianglify";
 
-  export let width = 1000;
-  export let height = 1000;
-  export let cellSize = 100;
-  export let seed = "example-seed";
+  export let width: number = 1000;
+  export let height: number = 1000;
+  export let cellSize: number = 100;
+  export let seed: string = "example-seed";
 
   let patternCanvas: HTMLCanvasElement
 

45dedd3c6791029412f24702f70ae3f37811730b
kazu0123 <kokubo.kazuki01.23@gmail.com>
2025-02-08 13:58:46 +0900

    remove trianglify

--- src/lib/Pattern.svelte
+++ src/lib/Pattern.svelte
@@ -1,37 +1,10 @@
-<script lang="ts">
-	import { onMount } from "svelte";
-  import trianglify from "trianglify";
-
-  interface Props {
-    width?: number;
-    height?: number;
-    cellSize?: number;
-    seed?: string;
-  }
-
-  let {
-    width = 1000,
-    height = 1000,
-    cellSize = 100,
-    seed = "example-seed"
-  }: Props = $props();
-
-  let patternCanvas: HTMLCanvasElement = $state()
-
-  function generatePattern() {
-    const pattern = trianglify({ width, height, cellSize, seed, });
-    pattern.toCanvas(patternCanvas);
-  }
-
-  onMount(generatePattern);
-</script>
-
 <style>
-  canvas {
+  .pattern {
     width: 100%;
     height: 100%;
     object-fit: cover;
+    background: linear-gradient(60deg, rgb(194, 194, 194), rgb(230, 230, 230));
   }
 </style>
 
-<canvas bind:this={patternCanvas}></canvas>
+<div class="pattern"></div>
